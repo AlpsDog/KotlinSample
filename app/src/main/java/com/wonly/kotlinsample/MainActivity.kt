5c8fdbf449f60b4f02d8a69f3c0acb227af74efb
houshengli <1414006272@qq.com>
2020-12-08 10:11:40 +0800

    侯胜利 12-8开始学习kotlin，誓做有志青年!!!!

---
+++ app/src/main/java/com/wonly/kotlinsample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.wonly.kotlinsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @Author: HSL
+ * @Time: 2020/12/8 10:09
+ * @E-mail: xxx@163.com
+ * @Description: 主页
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
