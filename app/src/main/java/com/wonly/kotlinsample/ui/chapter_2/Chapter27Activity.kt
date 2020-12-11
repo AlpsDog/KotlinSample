6f0efe9d858f77ed03d63421d271370564f4ce72
houshengli <1414006272@qq.com>
2020-12-11 14:21:41 +0800

    侯胜利 kotlin 2.7 Boolean类型（暂无用例）

---
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter27Activity.kt
@@ -0,0 +1,18 @@
+package com.wonly.kotlinsample.ui.chapter_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wonly.kotlinsample.R
+
+/**
+ * @Author: HSL
+ * @Time: 2020/12/11 14:18
+ * @E-mail: xxx@163.com
+ * @Description: 2.7 Boolean类型
+ */
+class Chapter27Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter27)
+    }
+}
