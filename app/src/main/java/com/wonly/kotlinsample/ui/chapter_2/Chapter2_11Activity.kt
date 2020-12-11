b5e636140a63b6dffcb63e49a4f5e15207c2df5a
houshengli <1414006272@qq.com>
2020-12-11 18:13:16 +0800

    侯胜利 kotlin 2.11 待补充...

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter2_11Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter2_11Activity.kt
@@ -1,19 +1,13 @@
 package com.wonly.kotlinsample.ui.chapter_2
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.wonly.kotlinsample.R
 
-/**
- * @Author: HSL
- * @Time: 2020/12/11 18:10
- * @E-mail: xxx@163.com
- * @Description: 类型别名
- */
-class Chapter210Activity : AppCompatActivity() {
+class Chapter2_11Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chapter210)
+        setContentView(R.layout.activity_chapter2_11)
     }
 }
