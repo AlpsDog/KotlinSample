a4a8ca86cdc60c956318a9d09f664a01b43e54d3
houshengli <1414006272@qq.com>
2020-12-11 16:18:42 +0800

    侯胜利 kotlin 2.8.4 Elvis运算

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter284Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter284Activity.kt
@@ -11,8 +11,28 @@ import com.wonly.kotlinsample.R
  * @Description: 2.8.4 Elvis运算
  */
 class Chapter284Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter284)
+        test()
+    }
+
+    /**
+     * Elvis运算其实就是 if else的简化写法
+     * 运算符： "?:"
+     * 含义： 左边表达式不为null,则返回左边表达式的值，否则返回右边表达式的值
+     */
+    fun test() {
+        var s: String? = "houshengli"
+
+        // 普通写法
+        var len = if (s != null) s.length else -1
+        println(len)
+
+        // Elvis运算符
+        var len2 = s?.length ?: -1;
+        println(len2)
+
     }
 }
