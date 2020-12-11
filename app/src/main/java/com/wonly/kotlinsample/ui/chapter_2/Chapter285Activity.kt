78fb27f831269e9717f727589a54e455c53e93ba
houshengli <1414006272@qq.com>
2020-12-11 16:32:30 +0800

    侯胜利 kotlin 2.8.5 可空类型强制调用属性或者方法

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter285Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter285Activity.kt
@@ -11,8 +11,24 @@ import com.wonly.kotlinsample.R
  * @Description: 2.8.5 强制调用
  */
 class Chapter285Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter285)
+        test()
+    }
+
+    /**
+     * 强制调用
+     * 不管变量是否为null,都可以直接调用该变量的属性或者方法
+     * 使用 "!!."
+     */
+    fun test() {
+        var s: String? = "houshengli";
+//        println(s.length);// 报错，不能直接调用
+        println(s!!.length);
+
+//        s = null;
+//        println(s!!.length);// 引发异常
     }
 }
