d4b06aa169f1b45110595033deec51d41343a811
houshengli <1414006272@qq.com>
2020-12-11 18:09:27 +0800

    侯胜利 kotlin 2.9.3 字符串方法

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter293Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter293Activity.kt
@@ -16,4 +16,28 @@ class Chapter293Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter293)
     }
+
+    /**
+     * 学习Kotlin String的方法
+     * 更多方法请百度
+     */
+    fun test() {
+        // 字符串转为Double
+        val s1 = "2.14"
+        val d: Double = s1.toDouble();
+        println(d)
+
+        // 转为Int
+        val s2 = "45"
+        val d2: Int = s2.toInt()
+        println(d2)
+
+        val s3 = "houshengli"
+        println(s3.capitalize()) // 首字母大写
+        println(s3.decapitalize()) // 首字母小写
+
+        // contains支持正则
+        var s4 = "java886"
+        println(s4.contains(Regex("\\d{3}")))
+    }
 }
