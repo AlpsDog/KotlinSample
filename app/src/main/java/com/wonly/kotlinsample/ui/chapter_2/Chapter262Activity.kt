84e8e4b12d577a06538006c00798e2d766b2a49b
houshengli <1414006272@qq.com>
2020-12-11 13:49:52 +0800

    侯胜利 kotlin 2.6.2 浮点型与整型直接的转换

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter262Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter262Activity.kt
@@ -1,8 +1,9 @@
 package com.wonly.kotlinsample.ui.chapter_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.wonly.kotlinsample.R
+
 /**
  * @Author: HSL
  * @Time: 2020/12/11 10:03
@@ -10,8 +11,15 @@ import com.wonly.kotlinsample.R
  * @Description: 浮点型与整型之间的转换
  */
 class Chapter262Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter262)
+    }
+
+    fun test() {
+        // toDouble、toFloat
+        // 没什么可说的
+        var d: Double = 66.0;
     }
 }
