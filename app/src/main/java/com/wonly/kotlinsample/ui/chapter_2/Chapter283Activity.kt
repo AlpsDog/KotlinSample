5ad4152490dff784ae9c0d48d4390dc2a27f7422
houshengli <1414006272@qq.com>
2020-12-11 16:09:53 +0800

    侯胜利 kotlin 2.8.3 安全调用

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter283Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_2/Chapter283Activity.kt
@@ -1,8 +1,9 @@
 package com.wonly.kotlinsample.ui.chapter_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.wonly.kotlinsample.R
+
 /**
  * @Author: HSL
  * @Time: 2020/12/11 14:25
@@ -10,8 +11,21 @@ import com.wonly.kotlinsample.R
  * @Description: 2.8.3 安全调用
  */
 class Chapter283Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter283)
+        test()
+    }
+
+    /**
+     * 安全调用使用："?."
+     */
+    fun test() {
+        // 使用 .? 进行安全访问，当vStr空null时，也不会出现空指针
+        var vStr: String? = "houshengli"
+        println(vStr?.length)// 返回10
+        vStr = null
+        println(vStr?.length)// 返回null
     }
 }
