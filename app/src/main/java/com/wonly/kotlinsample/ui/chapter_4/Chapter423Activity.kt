9ac1dcfdfed1690eca4a73d539b85f26bd6c3565
houshengli <1414006272@qq.com>
2020-12-15 11:37:37 +0800

    侯胜利 kotlin 4.2.3 when分支语句

--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_4/Chapter423Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_4/Chapter423Activity.kt
@@ -24,16 +24,28 @@ class Chapter423Activity : AppCompatActivity() {
      * 1.不在需要case关键字
      * 2.case后面的":"改为"->"
      * 3.default改为else,更加明确
+     * 4.包含多条语句时，用代码块{}
+     * 5.三个小改进
+     *   5.1 when分支可以匹配多个值
+     *   5.2 when分支后的值不要求常量，可以是任意表达式
+     *   5.3 when分支对条件表达式的类型没有任何要求
      */
     fun test() {
 
-        var score: Char = 'B'
+        var levels = "DE";
+        var score: Char = 'D'
         when (score) {
-            'A' -> println("优秀")
-            'A' -> println("良好")
-            'A' -> println("中")
-            'A' -> println("及格")
-            else -> print("不及格")
+            'S', 'A' -> println("优秀") // 5.1
+            'B' -> {
+                println("良好")
+                println("还是有进步空间哦!")
+            }
+            in levels -> println("中") // 5.2
+            'F' -> println("及格")
+            else -> {
+                print("不及格")
+                println("得叫家长了!!MD")
+            }
         }
     }
 }
