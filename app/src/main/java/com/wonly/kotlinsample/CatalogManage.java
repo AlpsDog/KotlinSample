103d314974c450d1921d56fe9883a20fe4f4b046
houshengli <1414006272@qq.com>
2020-12-17 10:19:21 +0800

    侯胜利 kotlin 6.2.4 个数可变的参数

--- app/src/main/java/com/wonly/kotlinsample/CatalogManage.java
+++ app/src/main/java/com/wonly/kotlinsample/CatalogManage.java
@@ -38,6 +38,7 @@ import com.wonly.kotlinsample.ui.chapter_6.Chapter612Activity;
 import com.wonly.kotlinsample.ui.chapter_6.Chapter614Activity;
 import com.wonly.kotlinsample.ui.chapter_6.Chapter621Activity;
 import com.wonly.kotlinsample.ui.chapter_6.Chapter622Activity;
+import com.wonly.kotlinsample.ui.chapter_6.Chapter624Activity;
 
 import java.util.HashMap;
 
@@ -92,6 +93,7 @@ public class CatalogManage {
         catalogMap.put("6.1.4", Chapter614Activity.class);
         catalogMap.put("6.2.1", Chapter621Activity.class);
         catalogMap.put("6.2.2", Chapter622Activity.class);
+        catalogMap.put("6.2.4", Chapter624Activity.class);
     }
 
     public static Class<?> getChapterCls(String catalogNum) {
--- app/src/main/java/com/wonly/kotlinsample/ui/chapter_6/Chapter624Activity.kt
+++ app/src/main/java/com/wonly/kotlinsample/ui/chapter_6/Chapter624Activity.kt
@@ -8,12 +8,41 @@ import com.wonly.kotlinsample.R
  * @Author: HSL
  * @Time: 2020/12/16 19:56
  * @E-mail: xxx@163.com
- * @Description: 这个人太懒，没留下什么踪迹~
+ * @Description: 6.2.4 个数可变的参数
  */
 class Chapter624Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter624)
+        test()
     }
+
+    /**
+     * 1.Kotlin允许可变参数，在形参前加 "vararg" 修饰即可
+     * 2.可变参数可以在形参列表任意位置，但一个函数最多只能有一个可变参数
+     */
+    fun test() {
+        testVararg(5, "疯狂Android讲义", "疯狂Kotlin讲义", "疯狂Java讲义")
+        // 可变参数在前时，后面参数必须使用命名参数
+        testVararg2("疯狂Android讲义", "疯狂Kotlin讲义", "疯狂Java讲义", num = 5)
+        // 可变参数接收数组时，在传入的数组参数前添加"*"运算符
+        var arr = arrayOf("001", "002", "003")
+        testVararg(arr.size, *arr)
+    }
+
+    fun testVararg(a: Int, vararg books: String) {
+        for (book in books) {
+            println(book)
+        }
+        println(a)
+    }
+
+    fun testVararg2(vararg books: String, num: Int) {
+        for (book in books) {
+            println(book)
+        }
+        println(num)
+    }
+
 }
